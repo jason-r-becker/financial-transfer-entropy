ea020ba45f59f738d53a9bf8e19c913c4194fc21
jason-r-becker <jasonrichardbecker@gmail.com>
2019-03-08 23:31:22 -0800

    Added network graphs

--- fte/transfer_entropy.py
+++ fte/transfer_entropy.py
@@ -9,7 +9,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.optimize import minimize
-
+from scipy.stats import norm
+from tqdm import tqdm
 plt.style.use('fivethirtyeight')
 
 
@@ -265,7 +266,7 @@ class TransferEntropy:
                     bbox=box)
 
     def plot_corr(self, method='spearman', ax=None, figsize=(6, 6),
-                  fontsize=8):
+                  fontsize=8, cbar=True, labels=True):
         """
         Plot correlation of assets.
 
@@ -282,19 +283,27 @@ class TransferEntropy:
             Figure size.
         fontsize: int, default=8
             Fontsize for asset labels.
+        cbar: bool, default=True
+            If True include color bar.
+        labels: bool, default=False
+            If True include tick labels for x & y axis.
+            If False do not inlclude labels.
         """
         if ax is None:
             fig, ax = plt.subplots(1, 1, figsize=figsize)
 
         self._corr = self.data.corr(method)
-        sns.heatmap(self._corr, ax=ax, vmin=-1, vmax=1,
-                    xticklabels=True, yticklabels=True, cmap='coolwarm')
-        plt.setp(ax.get_xticklabels(), fontsize=fontsize)
-        plt.setp(ax.get_yticklabels(), fontsize=fontsize)
-        cbar = ax.collections[0].colorbar
-        ticks = np.linspace(-1, 1, 5)
-        cbar.set_ticks(ticks)
-        cbar.set_ticklabels(ticks)
+        sns.heatmap(self._corr, ax=ax, vmin=-1, vmax=1, cbar=cbar,
+                    xticklabels=labels, yticklabels=labels, cmap='coolwarm')
+        if labels:
+            plt.setp(ax.get_xticklabels(), fontsize=fontsize)
+            plt.setp(ax.get_yticklabels(), fontsize=fontsize)
+            
+        if cbar:
+            cbar_ax = ax.collections[0].colorbar
+            ticks = np.linspace(-1, 1, 5)
+            cbar_ax.set_ticks(ticks)
+            cbar_ax.set_ticklabels(ticks)
 
     def _transfer_entropy_function(self, data, X, Y, bins, shuffle):
         """
@@ -332,8 +341,8 @@ class TransferEntropy:
             data_matrix, bins=bin_edges, labels=False), (3, -1)).T
 
         if shuffle:
-            for j in range(3):
-                np.random.shuffle(bin_vals[:, j])
+            # Shuffle y_lag for randomized transfer entropy.
+            np.random.shuffle(bin_vals[:, 2])
 
         # Find frequency of occurce for each set of joint vectors.
         p_ilag = Counter(bin_vals[:, 1])
@@ -403,7 +412,8 @@ class TransferEntropy:
         else:
             return te
 
-    def compute_effective_transfer_entropy(self, bins=6, sims=25):
+    def compute_effective_transfer_entropy(self, bins=6, sims=25,
+                                           std_threshold=1, pbar=False):
         """
         Compute effective transfer entropy matrix. Returned matrix is
         directional such that asset on X-axis inluences next day transfer
@@ -416,7 +426,9 @@ class TransferEntropy:
         sims: int, default=25
             Number of simulations to use when estimating randomized
             transfer entropy.
-
+        pbar: bool, default=False
+            If True, show progress bar for simulations.
+            
         Returns
         -------
         ete: [n x n] nd.array
@@ -427,24 +439,39 @@ class TransferEntropy:
         self.compute_transfer_entropy(bins)
 
         # Compute and store randomized transfer entropy.
-        rte_tensor = np.zeros([self._n, self._n, sims])
-        for i in range(sims):
-            rte_tensor[:, :, i] = self.compute_transfer_entropy(
-                bins, shuffle=True, save=False)
-        rte = np.mean(rte_tensor, axis=2)
+        self.rte_tensor = np.zeros([self._n, self._n, sims])
+        if pbar:
+            for i in tqdm(range(sims)):
+                self.rte_tensor[:, :, i] = self.compute_transfer_entropy(
+                    bins, shuffle=True, save=False)
+        else:
+            for i in range(sims):
+                self.rte_tensor[:, :, i] = self.compute_transfer_entropy(
+                    bins, shuffle=True, save=False)
+        
+        # Peform significance test on ETE values.
+        cutoff = norm(0, 1).cdf(std_threshold)
+        ete = np.zeros([self._n, self._n])
+        for i in range(self._n):
+            for j in range(self._n):
+                if i == j:
+                    continue
+                te = self.te.iloc[i, j]
+                rte_array = self.rte_tensor[i, j, :]
+                z = norm(np.mean(rte_array), np.std(rte_array)).cdf(te)
+                if z > cutoff:
+                    ete[i, j] = te - np.mean(rte_array)
+
+        rte = np.mean(self.rte_tensor, axis=2)
         self.rte = pd.DataFrame(rte, columns=self.assets, index=self.assets)
-
-        # Compute effective transfer entropy.
-        self.ete = self.te - self.rte
-
+        self.ete = pd.DataFrame(ete, columns=self.assets, index=self.assets)
+        
         # Store max and min values.
         self._te_max = np.max(self.te.values)
         self._te_min = np.min(self.ete.values)
 
-
-
     def plot_te(self, te='te', labels=True, cbar=True, ax=None,
-                figsize=(6, 6), fontsize=6):
+                figsize=(6, 6), fontsize=6, vmin=None, vmax=None):
         """
         Plot correlation of assets.
 
@@ -469,22 +496,28 @@ class TransferEntropy:
         if ax is None:
             fig, ax = plt.subplots(1, 1, figsize=figsize)
 
-
+        vmin = self._te_min if vmin is None else vmin
+        vmax = self._te_max if vmax is None else vmax
+        
         heatmap = eval(f'self.{te}')
-        sns.heatmap(heatmap, ax=ax, vmin=self._te_min, vmax=self._te_max,
+        sns.heatmap(heatmap, ax=ax, vmin=vmin, vmax=vmax,
                     cmap='viridis', xticklabels=labels, yticklabels=labels,
                     cbar=cbar)
         if labels:
             plt.setp(ax.get_xticklabels(), fontsize=fontsize)
             plt.setp(ax.get_yticklabels(), fontsize=fontsize)
 
-    def plot_corr_network(self, threshold=0.9, method='spearman',
-        ax=None, figsize=(12, 12), cmap='Blues', fontsize=8):
+    def plot_corr_network(self, network_type='circle', threshold=0.8,
+        method='pearson', ax=None, figsize=(12, 12), cmap='Reds', fontsize=8):
         """
         Plot correlation of assets.
 
         Parameters
         ----------
+        network_type: {'circle', 'cluster'}, default='circle'
+            Type of network graph.
+                - 'circle': Circular graph in decreasing node strength order.
+                - 'cluster': Spring graph of clusters.
         threshold: int, default=0.9
             Lower threshold of link strength for connections in network graph.
         method: {'spearman', 'pearson', 'kendall'}, default='spearman'
@@ -503,66 +536,221 @@ class TransferEntropy:
         """
         if ax is None:
             fig, ax = plt.subplots(1, 1, figsize=figsize)
-            
+        
         # Find correlation matrix and transform it in a links data frame.
         corr = self.data.corr(method)
         links = corr.stack().reset_index()
-        links.columns = ['var1', 'var2','val']
-        links = links.loc[
-            (links['var1'] != links['var2']) & (links['val'] > threshold)]
+        links.columns = ['in', 'out', 'weight']
+        links = links.loc[(links['in'] != links['out'])
+                          & (np.abs(links['weight']) > threshold)]
+        links = links[['out', 'in', 'weight']].reset_index(drop=True)
 
+        # Subset to only use upper triangle portion of correlation matrix.
+        edges = defaultdict(int)
+        ix = []
+        for i, row in links.iterrows():
+            if (edges[(row['out'], row['in'])]
+                + edges[(row['in'], row['out'])] > 0):
+                continue
+            else:
+                edges[(row['out'], row['in'])] += 1
+                ix.append(i)
+        links = links.loc[ix]
+        
         # Find node centrality.
         centrality = defaultdict(int)
         for _, row in links.iterrows():
-            centrality[row['var1']] += row['val']
+            centrality[row['out']] += row['weight']
+            centrality[row['in']] += row['weight']
 
-        # Sort by node centrality.
+        # Sort by node centrality, make node strength range [0.2, 1].
         nodes = sorted(centrality, key=centrality.get, reverse=True)
         ns = np.array([centrality[node] for node in nodes])
-        node_strengths = (ns - np.min(ns)) / (np.max(ns) - np.min(ns))
-
+        node_strengths = 0.2 + 0.8 * (ns-np.min(ns)) / (np.max(ns)-np.min(ns))
+        
         # Build OrderedGraph of nodes by centrality measure.
         G = nx.OrderedGraph()
         G.add_nodes_from(nodes)
-        # Use temporary graph to add proper edges.
-        G_temp = nx.from_pandas_edgelist(links, 'var1', 'var2')
-        G.add_edges_from(G_temp.edges)
-
-        # Find edge strengths.
-        edge_strengths = np.array([
-            float(links.loc[
-                (links['var1']==e[0]) & (links['var2']==e[1])]['val']) \
-            for e in list(G.edges)
-            ])
+        G.add_weighted_edges_from(list(links.itertuples(index=False)))
+        
+        # Networkx automatically scales color scheme and node size to make the
+        # minimum 0, resulting in no color or circle node for weakest
+        # connection/node. Fix this by adding a blank node with strenth of 0
+        # and edge strength of 80% of smallest edge to largest node.
+        G.add_node('')
+        G.add_edge('', nodes[0], weight=0.8*np.min(links['weight']))
+        node_strengths = np.append(node_strengths, 0)
+        
+        # Find edge weights.
+        edge_weights = np.array([G.edges[e]['weight'] for e in list(G.edges)])
 
         # Plot network graph.
-        nx.draw_circular(G, with_labels=True,
-            cmap=cmap, node_size=1000*node_strengths, node_color=node_strengths,
-            edge_cmap=plt.get_cmap(cmap), edge_color=edge_strengths,
-            alpha=0.8, font_color='k', linewidths=1, font_size=fontsize)
+        kwargs = {
+            'ax': ax,
+            'with_labels': True,
+            'cmap': cmap,
+            'node_size': 1000*node_strengths,
+            'node_color': node_strengths,
+            'edge_cmap': plt.get_cmap(cmap),
+            'edge_color': edge_weights,
+            'alpha': 0.8,
+            'font_color': 'k',
+            'linewidths': 1,
+            'font_size': fontsize,
+            }
+        
+        if network_type == 'circle':
+            nx.draw_circular(G, **kwargs)
+        elif network_type == 'cluster':
+            nx.draw(G, **kwargs)
 
+    def plot_ete_network(self, network_type='circle', node_value='out',
+        threshold=0.01,  ax=None, figsize=(12, 12), cmap='Blues', fontsize=8):
+        """
+        Plot correlation of assets.
 
+        Parameters
+        ----------
+        network_type: {'circle', 'cluster'}, default='circle'
+            Type of network graph.
+                - 'circle': Circular graph in decreasing node strength order.
+                - 'cluster': Spring graph of clusters.
+        node_value: {'out', 'in'}, default='out'
+            Transfer entropy node centraily measure.
+                'out': Total transfer entropy out of each node.
+                'in': Total transfer entropy in to each node.
+        threshold: int, default=0.01
+            Lower threshold of link strength for connections in network graph.
+        ax: matplotlib axis, default=None
+            Matplotlib axis to plot figure, if None one is created.
+        figsize: list or tuple, default=(6, 6)
+            Figure size.
+        cmap: str, default='Blues'
+            Matploblib cmap.
+        fontsize: int, default=8
+            Fontsize for asset labels.
+        """
+        if ax is None:
+            fig, ax = plt.subplots(1, 1, figsize=figsize)
 
+        # Compute effective transfer entropy DataFrame and
+        # transform it in a links data frame.
+        links = self.ete.stack().reset_index()
+        links.columns = ['in', 'out', 'weight']
+        links = links.loc[links['weight'] > threshold]
+        links = links[['out', 'in', 'weight']]
+        if len(links) < 1:
+            msg = 'Threshold is too high, lower threshold below '
+            msg += f'{np.max(self.ete.values):.4f}'
+            raise ValueError(msg)
+            
+        # Find node centrality.
+        all_nodes = list(set(list(links['out']) + list(links['in'])))
+        centrality = {node: 0 for node in all_nodes}
+        for _, row in links.iterrows():
+            centrality[row[node_value]] += row['weight']
+        
+        # Sort by node centrality, make node strength range [0.2, 1].
+        nodes = sorted(centrality, key=centrality.get, reverse=True)
+        ns = np.array([centrality[node] for node in nodes])
+        node_strengths = 0.2 + 0.8 * (ns-np.min(ns)) / (np.max(ns)-np.min(ns))
+        
+        # Build Ordered Directed Graph of nodes by centrality measure.
+        G = nx.OrderedDiGraph()
+        G.add_nodes_from(nodes)
+        G.add_weighted_edges_from(list(links.itertuples(index=False)))
+        
+        # Networkx automatically scales color scheme and node size to make the
+        # minimum 0, resulting in no color or circle node for weakest
+        # connection/node. Fix this by adding a blank node with strenth of 0
+        # and edge strength of 80% of smallest edge to largest node.
+        G.add_node('')
+        G.add_edge('', nodes[0], weight=0.8*np.min(links['weight']))
+        node_strengths = np.append(node_strengths, 0)
+        
+        # Find edge weights.
+        edge_weights = np.array([G.edges[e]['weight'] for e in list(G.edges)])
+        
+        # Plot network graph.
+        kwargs = {
+            'ax': ax,
+            'with_labels': True,
+            'cmap': cmap,
+            'node_size': 1000*node_strengths,
+            'node_color': node_strengths,
+            'edge_cmap': plt.get_cmap(cmap),
+            'edge_color': edge_weights,
+            'alpha': 0.8,
+            'font_color': 'k',
+            'linewidths': 1,
+            'font_size': fontsize,
+            }
+        
+        if network_type == 'circle':
+            nx.draw_circular(G, **kwargs)
+        elif network_type == 'cluster':
+            nx.draw(G, **kwargs)
 
-# self = TransferEntropy()
+# eqs = 'SPY XLK XLV XLF IYZ XLY XLP XLI XLE XLU IYR XLB'\
+#     ' DIA IWM ECH EWW EWC EWZ'.split()
+# fi = 'AGG SHY IEI IEF TLT TIP LQD HYG MBB'.split()
+# cmdtys = 'GLD SLV DBA DBC USO UNG'.split()
+# assets = eqs + fi + cmdtys
+#
+# assets2 = 'USO GLD XLE XLK'.split()
+# self = TransferEntropy(assets=assets2)
 # len(self.assets)
-
-# %%
-# Set Period.
-# start = '1/2/2015'
-# end = '12/31/2016'
+# #
+# # # %%
+# # Set Period.
+# start = '1/2/2013'
+# end = '12/31/2013'
 # self.set_timeperiod(start, end)
+# #
+# # %%
+# # Plot correlation matrix.
+# self.plot_corr(cbar=True)
+# plt.show()
+#
+# # %%
+# # Find network graph coordinates.
+# self.plot_corr_network(network_type='circle', threshold=0.65)
+# plt.show()
+#
+# self.plot_corr_network(network_type='cluster', threshold=0.65)
+# plt.show()
+#
+# # %%
+# # Compute effective transfer entropy.
+# self.compute_effective_transfer_entropy(sims=1000, pbar=True)
+
 
 # %%
-# Plot correlation matrix.
-# self.plot_corr('spearman')
+
+# # %%
+# # Plot effective transfer entropy.
+# self.plot_te(te='ete', vmax=0.5*np.max(self.te.values))
+# plt.show()
+#
+# # %%
+# # Find network graph coordinates.
+# thresh = 0.03
+# self.plot_ete_network(network_type='circle', threshold=thresh)
+# plt.show()
+#
+# self.plot_ete_network(network_type='cluster', threshold=thresh)
+# plt.show()
+#
+# # %%
+# self.plot_ete_network(network_type='circle', node_value='in', threshold=thresh,
+#                       cmap='Purples')
+# plt.show()
+#
+# self.plot_ete_network(network_type='cluster', node_value='in', threshold=thresh,
+#                       cmap='Purples')
 # plt.show()
 
 # %%
-# Find network graph coordinates.
-# self.plot_corr_network(threshold=0.88)
-# plt.show()
-
 # --------------------- Still in-progress work below ----------------------- #
 
 # # %%
@@ -587,19 +775,8 @@ class TransferEntropy:
 # plt.show()
 #
 #
-# # %%
-# self.plot_asset_graph(0.6)
-# # %%
-# # Plot Transfer Entropy.
-# self.compute_transfer_entropy(bins=6)
-# self.plot_te()
-# plt.show()
-#
-# # %%
-# # Compute Effective Transfer Entropy.
-# # *** Takes ~10 sec per simulaion with 6 bins ***
-# self.compute_effective_transfer_entropy(bins=6, sims=5)
-#
+
+
 # # %%
 # # Plot
 # fig, axes = plt.subplots(1, 3, figsize=[14, 4])
@@ -608,11 +785,5 @@ class TransferEntropy:
 #     ax.set_title(f'${te.upper()}_{{X \\rightarrow Y}}$')
 # plt.tight_layout()
 # plt.show()
-#
-#
-# # %%
-# fig, axes = plt.subplots(3, 4, figsize=[6,6])
-# fig = plt.figure()
-# plt.tight_layout()
 #
 #
